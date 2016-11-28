e7c0bf33a0bbd90b4a2259db164785d38bf4371d
root <root@iZj6cb2i6ipgckft6wzh87Z.(none)>
2016-11-28 23:33:11 +0800

    还原sidebar.php

--- themes/admin/common/sidebar.php
+++ themes/admin/common/sidebar.php
@@ -1,4 +1,5 @@
-            <div class="col-md-3">
+
+			<div class="col-md-3">
                 <div class="list-group">
                     <a href="" class="list-group-item disabled">管理面板</a>
 				    <a href="<?php echo site_url('admin/site_settings');?>" class="list-group-item">基本设置</a>
@@ -10,22 +11,4 @@
 					<a href="<?php echo site_url('admin/db_admin/index');?>" class="list-group-item">数据库管理</a>
 					<a href="http://www.startbbs.com" class="list-group-item">访问官方</a>
                 </div>
-		<div class="list-group">
-                	<div class="panel panel-default">
-                        	<div class="panel-heading">
-                                        <h3 class="panel-title">统计</h3>
-                                </div>
-                                <div class="panel-body">
-                                      <ul class="list-unstyled">
-                                                            <li>最新会员：<?php echo $stats['last_username']?></li>
-                                                            <li>注册会员： <?php echo $stats['total_users']?></li>
-                                                            <li>今日话题： <?php echo $stats['today_topics'];?></li>
-                                                            <li>昨日话题： <?php echo $stats['yesterday_topics'];?></li>
-                                                            <li>话题总数： <?php echo $stats['total_topics']?></li>
-                                                            <li>回复数： <?php echo $stats['total_comments']?></li>
-                                      </ul>
-                                 </div>
-                        </div>
-                </div>
-
             </div><!-- /.col-md-4 -->
